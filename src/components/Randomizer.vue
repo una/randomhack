f53f2c45aa8acd72955ef49493d8805f692d5cc9
Una Kravets <una.kravets@gmail.com>
2017-01-05 00:48:44 -0500

    swapping emoji for adjectives

--- src/components/Randomizer.vue
+++ src/components/Randomizer.vue
@@ -1,15 +1,23 @@
 <template>
   <ul class="randomized-list">
-    <li class="randomized-item"> {{ adj }} </li>
     <li class="randomized-item"> {{ noun }} </li>
+    <li class="randomized-item"> {{ emoji }} </li>
     <li class="randomized-item challenge-item"> with {{ challenge }} </li>
   </ul>
 </template>
 
 <script>
 const Sentencer = require('sentencer');
+const emojiList = require('emojilib/emojis.json');
+
+const randomEmoji = function (obj) {
+  var keys = Object.keys(obj)
+  return (obj[keys[keys.length * Math.random() << 0]])['char'];
+}
+
 let challengeList = [ 'Ruby', 'JavaScript', 'Node', 'GO', 'Python', 'React', 'Vue.js', 'Hardware', 'Stylus', 'Offline-first', 'CSS Modules', 'Hapi', 'an API' ];
 
+// configure randomized challenge item as Sentencer plugin
 Sentencer.configure({
   actions: {
     challenge: function () {
@@ -19,13 +27,14 @@ Sentencer.configure({
   }
 });
 
+// randomizer component
 export default {
   name: 'randomizer',
   data () {
     return {
-      adj: Sentencer.make('-{ adjective }-'),
       noun: Sentencer.make('-{ noun }-'),
-      challenge: Sentencer.make('-{ challenge }-')
+      challenge: Sentencer.make('-{ challenge }-'),
+      emoji: randomEmoji(emojiList)
     }
   }
 }
