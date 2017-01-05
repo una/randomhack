890bcb89482937c4cd204fb02638dd6f74ac12df
Una Kravets <una.kravets@gmail.com>
2017-01-05 00:30:36 -0500

    added dev challenge

--- src/components/Randomizer.vue
+++ src/components/Randomizer.vue
@@ -2,18 +2,30 @@
   <ul class="randomized-list">
     <li class="randomized-item"> {{ adj }} </li>
     <li class="randomized-item"> {{ noun }} </li>
+    <li class="randomized-item challenge-item"> with {{ challenge }} </li>
   </ul>
 </template>
 
 <script>
-var Sentencer = require('sentencer');
+const Sentencer = require('sentencer');
+let challengeList = [ 'Ruby', 'JavaScript', 'Node', 'GO', 'Python', 'React', 'Vue.js', 'Hardware', 'Stylus', 'Offline-first', 'CSS Modules', 'Hapi', 'an API' ];
+
+Sentencer.configure({
+  actions: {
+    challenge: function () {
+      let randomLang = challengeList[Math.floor(Math.random() * challengeList.length)];
+      return randomLang;
+    }
+  }
+});
 
 export default {
   name: 'randomizer',
   data () {
     return {
       adj: Sentencer.make('-{ adjective }-'),
-      noun: Sentencer.make('-{ noun }-')
+      noun: Sentencer.make('-{ noun }-'),
+      challenge: Sentencer.make('-{ challenge }-')
     }
   }
 }
@@ -27,5 +39,9 @@ export default {
 
   .randomized-item {
     display: inline-block;
+  }
+
+  .challenge-item {
+    background: yellow;
   }
 </style>
